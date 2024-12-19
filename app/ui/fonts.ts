a29acec8c63a0255a6cecb7886333f9cc221f783
MohadT <traore.mmt11@gmail.com>
2024-12-19 10:51:11 +0000

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,11 +2,4 @@ import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = (weight:? '400' | '700') => {
-    return {
-      fontFamily: '"Lusitana", serif',
-      fontWeight: weight || "400",
-      subsets: ['latin'],  // You can specify other subsets like 'latin-ext' if needed
-     
-    };
-  };
+export const lusitana = Lusitana ({subsets: ['latin']});
